8b17fe9c7d168667ddb8d08adafcacbbf6690948
batam2107 <tamleba01@gmail.com>
2024-07-01 20:19:03 +0700

    Update Customer.java

--- src/java/Models/Customer.java
+++ src/java/Models/Customer.java
@@ -97,4 +97,8 @@ public class Customer {
         this.status = status;
     }
 
+        public boolean isLocked() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
