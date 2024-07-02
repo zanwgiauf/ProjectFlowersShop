e3131e02880d2f610d8d9590e9cc6958340fb58e
TruongLeMin <TruongLMCE171886@fpt.edu.vn>
2024-07-02 14:18:32 +0700

    Update CategoryDAO.java them cac ham cua phan trang va bo loc

--- src/java/DAOs/CategoryDAO.java
+++ src/java/DAOs/CategoryDAO.java
@@ -110,4 +110,190 @@ public class CategoryDAO {
             e.printStackTrace();
         }
     }
+
+    public List<Product> getProductsByCategory(int categoryID) {
+        List<Product> products = new ArrayList<>();
+        String query = "SELECT * FROM Products WHERE categoryID = ?";
+        try ( PreparedStatement ps = conn.prepareStatement(query)) {
+            ps.setInt(1, categoryID);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                products.add(new Product(
+                        rs.getInt("productID"),
+                        rs.getString("name"),
+                        rs.getInt("price"),
+                        rs.getInt("reducedPrice"),
+                        rs.getInt("quantity"),
+                        rs.getString("description"),
+                        rs.getString("image"),
+                        rs.getInt("categoryID")
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return products;
+    }
+
+    public List<Product> getListByPage(List<Product> list,
+            int start, int end) {
+        ArrayList<Product> arr = new ArrayList<>();
+        for (int i = start; i < end; i++) {
+            arr.add(list.get(i));
+        }
+        return arr;
+    }
+
+    public List<Product> getProductByCategory(int category_id) {
+        List<Product> list = new ArrayList<>();
+        String sql = "SELECT p.categoryID, p.productID, p.name, p.price, p.reducedPrice, p.quantity, p.description, p.image \n"
+                + "FROM Products p\n"
+                + "INNER JOIN Categories c ON p.categoryID = c.categoryID\n"
+                + "WHERE p.categoryID = ?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, category_id);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                //Category category = new Category(category_id, rs.getString("name"));
+                Product product = new Product(
+                        rs.getInt("productID"),
+                        rs.getString("name"),
+                        rs.getInt("price"),
+                        rs.getInt("reducedPrice"),
+                        rs.getInt("quantity"),
+                        rs.getString("description"),
+                        rs.getString("image"),
+                        rs.getInt("categoryID")
+                );
+                list.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public List<Product> SearchAll(String text) {
+        List<Product> list = new ArrayList<>();
+        String sql = "SELECT c.name, p.productID, p.name, p.price, p.description, p.quantity, p.image "
+                + "FROM Products p "
+                + "INNER JOIN Categories c ON p.categoryID = c.categoryID "
+                + "WHERE p.name LIKE ? OR c.name LIKE ?";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, "%" + text + "%");
+            ps.setString(2, "%" + text + "%");
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Category category = new Category(rs.getInt("categoryID"), rs.getString("name"));
+                Product product = new Product(
+                        rs.getInt("productID"),
+                        rs.getString("name"),
+                        rs.getInt("price"),
+                        rs.getInt("reducedPrice"),
+                        rs.getInt("quantity"),
+                        rs.getString("description"),
+                        rs.getString("image"),
+                        rs.getInt("categoryID")
+                );
+                list.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public List<Product> getProductLow() {
+        List<Product> list = new ArrayList<>();
+        String sql = "SELECT * FROM Products ORDER BY price ASC";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Category category = new Category(rs.getInt("categoryID"), rs.getString("name"));
+                Product product = new Product(
+                        rs.getInt("productID"),
+                        rs.getString("name"),
+                        rs.getInt("price"),
+                        rs.getInt("reducedPrice"),
+                        rs.getInt("quantity"),
+                        rs.getString("description"),
+                        rs.getString("image"),
+                        rs.getInt("categoryID")
+                );
+                list.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public List<Product> getProductHigh() {
+        List<Product> list = new ArrayList<>();
+        String sql = "SELECT * FROM Products ORDER BY price DESC";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Category category = new Category(rs.getInt("categoryID"), rs.getString("name"));
+                Product product = new Product(
+                        rs.getInt("productID"),
+                        rs.getString("name"),
+                        rs.getInt("price"),
+                        rs.getInt("reducedPrice"),
+                        rs.getInt("quantity"),
+                        rs.getString("description"),
+                        rs.getString("image"),
+                        rs.getInt("categoryID")
+                );
+                list.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public List<Product> getProductAZ() {
+        List<Product> list = new ArrayList<>();
+        String sql = "SELECT * FROM Products ORDER BY name";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Category category = new Category(rs.getInt("categoryID"), rs.getString("name"));
+                Product product = new Product(
+                        rs.getInt("productID"),
+                        rs.getString("name"),
+                        rs.getInt("price"),
+                        rs.getInt("reducedPrice"),
+                        rs.getInt("quantity"),
+                        rs.getString("description"),
+                        rs.getString("image"),
+                        rs.getInt("categoryID")
+                );
+                list.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
 }
